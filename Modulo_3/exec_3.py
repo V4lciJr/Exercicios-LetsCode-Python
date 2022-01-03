6e0cab16368ac93a84ef63ff2cf02ff3b291c463
Valci Júnior <valcijunior19@hotmail.com>
2022-01-02 21:22:01 -0300

    inicio da resolucao do exercicio 3

--- Modulo_3/exec_3.py
+++ Modulo_3/exec_3.py
@@ -11,6 +11,17 @@ from exec_2 import csv, ler_arq_csv, cria_copia_csv
 
 
 def media_de_notas_csv(local_arquivo, novo_arquivo):
-    pass
+
+    arquivo_lido = ler_arq_csv(local_arquivo)
+
+    soma = 0
+    media = 0.0
+    for linha in arquivo_lido[1:]:
+        for nota in linha[3:]:
+            soma += float(nota)
+        media = soma / len(linha[3:])
+        soma = 0
+        print(f'{media:.2f}')
 
 
+media_de_notas_csv('..\\alunos.csv', None)
